60526676f930822ba32dbfd5e58f138cd52f3505
FedorBysov <bysov1987@mail.ru>
2024-10-05 16:32:36 +0300

    Create Multiplication table 2.3

--- app/src/main/java/com/example/jcstepik/MainActivity.kt
+++ app/src/main/java/com/example/jcstepik/MainActivity.kt
@@ -3,13 +3,24 @@ package com.example.jcstepik
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jcstepik.ui.theme.JCStepikTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +33,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    TimesTable("Android")
                 }
             }
         }
@@ -30,17 +41,42 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TimesTable(name: String, modifier: Modifier = Modifier) {
+    Column(modifier = Modifier
+        .padding(4.dp)
+        .fillMaxSize()
+
+        ) {
+        for (i in 1 until 10){
+            Row(modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+            ) {
+                for (j in 1 until 10){
+                    var background = if((i+j)%2 == 0) Color.Yellow else Color.White
+                    Box(modifier = Modifier
+                        .border(1.dp, color = Color.Black)
+                        .fillMaxHeight()
+                        .weight(1f)
+                        .background(color = background),
+                        contentAlignment = Alignment.Center
+                    ){
+                        Text("${i*j}")
+                    }
+
+                }
+            }
+        }
+    }
 }
+
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun PreviewTimesTable() {
     JCStepikTheme {
-        Greeting("Android")
+        TimesTable("Android")
     }
+
 }
